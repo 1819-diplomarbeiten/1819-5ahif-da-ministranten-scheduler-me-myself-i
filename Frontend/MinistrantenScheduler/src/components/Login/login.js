4b846e778d14f6fe8b72411cd4a22c2a3df1c47d
Patrick Mistlberger <patrickm99@hotmail.de>
2018-11-16 13:55:05 +0100

    Login Layout
    login layout fertig evtl. feinschliff der farben

--- Frontend/MinistrantenScheduler/src/components/Login/login.js
+++ Frontend/MinistrantenScheduler/src/components/Login/login.js
@@ -4,31 +4,91 @@ import {html, LitElement} from '@polymer/lit-element';
 export class Login extends LitElement{
     render() {
         return html`
-            <div class="container text-md-center">
-                <table>
-                    <tr>
-                        <td>
-                            <label>Username: </label>
-                        </td>
-                        <td>
-                            <input class="form-control" id="userName"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Passwort: </label>
-                        </td>
-                        <td>
-                            <input class="form-control" id="[[password]]"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button @click="${() => this.checkLogin()}">Login</button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
+	<script lang="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script lang="javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="/src/components/Login/designInput/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/css/main.css">
+<!--===============================================================================================-->	
+	<div class="limiter">
+		<div class="container-login100 background_size">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-logo">
+						<i class="zmdi zmdi-landscape"></i>
+					</span>
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						Log in
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+
+					<div class="contact100-form-checkbox">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+						<label class="label-checkbox100" for="ckb1">
+							Remember me
+						</label>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-90">
+						<a class="txt1" href="#">
+							Forgot Password?
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<!--<script src="/src/components/Login/designInput/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/src/components/Login/designInput/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/src/components/Login/designInput/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/src/components/Login/designInput/vendor/daterangepicker/moment.min.js"></script>
+	<script src="/src/components/Login/designInput/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="/src/components/Login/designInput/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="/src/components/Login/designInput/js/main.js"></script>
+	<script src="styles.css"></script>
+	
         `;
     }
 
