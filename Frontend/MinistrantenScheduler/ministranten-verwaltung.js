c9f7b5c439455f03968a697b428fbd50681d78e3
Patrick Mistlberger <patrickm99@hotmail.de>
2018-12-19 13:01:08 +0100

    Login & Verkettung
    Login wurde neu überarbeitet und responsive
    Verkettung zur ersten View über ministranten-verwaltung

--- Frontend/MinistrantenScheduler/ministranten-verwaltung.js
+++ Frontend/MinistrantenScheduler/ministranten-verwaltung.js
@@ -9,15 +9,56 @@ import {html, LitElement} from '@polymer/lit-element';
  * @demo demo/index.html
  */
 class MinistrantenVerwaltung extends LitElement {
+
+
+    static get properties() {
+        return {
+            switcher: this.shadowRoot
+        }
+    }
+
+    constructor() {
+        super();
+        this.switcher = this.shadowRoot;
+
+    }
+
+
+    switchComponent(input) {
+        this.switcher = this.switcher.getElementById('components');
+        switch (input.toString()) {
+            case "login":
+
+            case "chooser":
+
+        }
+        if (input == "login") {
+            this.switcher.innerHTML = `<login-component></login-component>`;
+        }
+        else if (input == "church-event") {
+            this.switcher.innerHTML = `<church-event-component></church-event-component>`
+        }
+
+
+
+    }
+
+
+/*<!--<create-event-component></create-event-component>-->
+<!--<create-church-event-component></create-church-event-component>-->
+<!--<user-settings-component></user-settings-component>-->
+<!--<participant-settings-component></participant-settings-component>-->*/
+
+
+
+
   render() {
+    $(document).ready(() =>{
+        this.switchComponent("church-event")
+    });
     return html`
       <div id="components">
-        <church-event-component></church-event-component>
-        <!--<login-component></login-component>-->
-        <!--<create-event-component></create-event-component>-->
-        <!--<create-church-event-component></create-church-event-component>-->
-        <!--<user-settings-component></user-settings-component>-->
-        <!--<participant-settings-component></participant-settings-component>-->
+        
       </div>
     `;
   }
--- Frontend/MinistrantenScheduler/src/components/Login/login.js
+++ Frontend/MinistrantenScheduler/src/components/Login/login.js
@@ -5,7 +5,9 @@ export class Login extends LitElement {
 
 
     checkLogin() {
-        console.log("click event login" + password);
+        var li = this.shadowRoot.getElementById('username').value;
+        li += ` `+this.shadowRoot.getElementById('password').value;
+        console.log("click event login :" + li );
     }
 
     static get properties() {
@@ -19,55 +21,40 @@ export class Login extends LitElement {
     render() {
         return html`
         <!--======================================Wichtig==================================================-->
-        <script lang="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
         <script lang="javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script lang="javascript" type="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
         <!--===============================================================================================-->
-        <link type="text/css" rel="stylesheet" href="/src/components/Login/designInput/styles/styles.css">
-        <link type="text/css" rel="stylesheet" href="/src/components/Login/designInput/styles/utils.css">
-        <link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="/src/components/Login/designInput/fonts/iconic/css/material-design-iconic-font.min.css">
+        <link rel="stylesheet" type="text/css" href="/src/components/Login/styles.css">
         
         
-        <div class="limit backgrond-img">
-            <div class="container-login">
-                <div class="wrap-login"><!--font anmeldung-->
-                    <form class="login-form validate-form">
-                        <span class="login-form-logo icon-set">
-                            <i class=""></i>
-                        </span>
-                        
-                        <span class="login-form-title p-b-34 p-t-27">
-                            Log in
-                        </span>
-                        
-                        <div class="wrap-input validate-input" data-validate="Enter username">
-                            <input class="input" type="text" name="username" placeholder="Username">
-                            <span class="focus-input" data-placeholder="&#xf207;"></span>
+        
+        
+        
+        
+        
+            <div class="container">
+	            <section id="content">
+		            <form action="">
+			            <h1>Login</h1>
+			            <div class="form-input">
+			            <div>
+				            <input type="text" placeholder="Username" required="" id="username"/>
+			            </div>
+			            <div>
+				            <input type="password" placeholder="Password" required="" id="password" />
+			            </div>
+			            <div>
+				            <input type="submit" value="Log in"  @click="${() => this.checkLogin()}"/>
+			            </div>
+			            <div class="lost-style">
+			                <a href="#">Lost your password?</a> 
                         </div>
-                        
-                        <div class="wrap-input validate-input" data-validate="Enter password">
-                            <input class="input" type="password" name="pass" placeholder="Password"><!--name für url Anzeige-->
-                            <span class="focus-input" data-placeholder="&#xf207;"></span>
                         </div>
-                        
-                        <div class="contact-form-checkbox">
-                            <input class="input-checkbox" id="mindMe" type="checkbox" name="remember-me">
-                            <label class="label-checkbox" for="mindMe">Remember me</label>
-                        </div>
-                        
-                        <div class="container-login-form-btn">
-                            <button class="login-form-btn" @click="${() => alert("Hallo")}">Login</button>
-                        </div>
-                        
-                        <div class="text-center p-t-90">
-                            <a class="txtl" href="#">Forgot Password?</a>
-                        </div>
-                        
-                    </form>
-                </div>
-            </div>
-        </div>       
+		            </form><!-- form -->
+	            </section><!-- content -->
+            </div><!-- container -->
+       
         `;
     }
 }
