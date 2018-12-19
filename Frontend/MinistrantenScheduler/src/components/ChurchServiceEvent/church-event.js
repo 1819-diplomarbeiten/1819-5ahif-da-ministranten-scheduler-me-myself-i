ccc66e2d960e59900e8a00b8ccd34a7c632c806e
Patrick Mistlberger <patrickm99@hotmail.de>
2018-12-19 10:34:47 +0100

    Drag & Drop
    Drag funktioniert 
    Drop leider nicht

--- Frontend/MinistrantenScheduler/src/components/ChurchServiceEvent/church-event.js
+++ Frontend/MinistrantenScheduler/src/components/ChurchServiceEvent/church-event.js
@@ -1,19 +1,19 @@
 import {html, LitElement} from '@polymer/lit-element';
 import {HtmlService} from '../../services/HtmlClientService/html-service';
-import {Participant} from "../Entities/Participant/participant";
 import {repeat} from "@polymer/lit-element/node_modules/lit-html/directives/repeat.js";
-//import {PolymerElement, html} from 'https://unpkg.com/@polymer/polymer/polymer-element.js?module';
-//import {Sortable, Plugins} from '@shopify/draggable';
+
 
 
 export class ChurchServiceEvent extends LitElement{
+
 
 
     static get properties() {
         return{
             allParticipants:{
                 type: Array
-            }
+            },
+            internalDND: Number
         }
     }
 
@@ -21,68 +21,40 @@ export class ChurchServiceEvent extends LitElement{
     constructor() {
         super();
         this.allParticipants = HtmlService.getAllParticipant();
+        //this.internalDND = "Dragger";
+        //this.init()
+    }
+
+     dragStartHandler(event) {
+         console.log("drag" + event)
+         this.internalDND = event
+     }
+
+    dropHandler() {
+        console.log("drop"+ this.internalDND)
+        this.shadowRoot.getElementById('test').innerHTML += `<p>${this.internalDND}</p>`
 
     }
 
-    dragStart(event) {
-        console.log("Hallo");
-        alert(event.target.id);
-        //event.dataTransfer.setData("id",event.target.id);
-    }
-    allowDrop(event) {
-        event.preventDefault();
-    }
-    drop(event) {
-        event.preventDefault();
-        var data = event.dataTransfer.getData("id");
-        event.target.appendChild(document.getElementById(data));
+    dragOverHandler() {
+         console.log("dragover")
     }
 
 
 
 
 
-
-    /**********************************************Appointment*********************************************************/
-    loadAppointments() {
-        this.shadowRoot.getElementById('Appo').innerHTML = this.addAppointmentLayout();
-    }
-
-    /**********************************************Appointment-Layout**************************************************/
-
-    addAppointmentLayout() {
-        return `
-         <div>
-            <div>
-                <div class="rectangle-Appo rconers">
-                    <div class="row font-header-style">
-                        <label>Montag</label>
-                        <hr>
-                    </div>                
-                    <div style="height: auto;width: auto;" id="test" ondrop="drop(event)" ondragover="allowDrop(event)">
-                        
-                    </div>
-                </div>
-            </div>
-         </div>
-        
-          `;
-    }
-    /**********************************************Appointment*********************************************************/
 
 
 
     /******************************************Render******************************************************************/
     render() {
-        $(document).ready(() =>{
-            this.loadAppointments();
-        });
         return html`
-        <script lang="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
         <script lang="javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">        
+        <script lang="javascript" type="javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">      
         <link rel="stylesheet" type="text/css" href="/src/components/ChurchServiceEvent/styles.css">
-        
+        <!--<script lang="javascript" type="javascript" src="/node_modules/@shopify/draggable/lib/draggable.js"></script>-->
         
         
         
@@ -90,22 +62,29 @@ export class ChurchServiceEvent extends LitElement{
             <h1>Drag&Drop</h1>
         </div>  
         <br>
-        <div>
-            <div class="col-md-3 row">             
-                <div id="participants" class="col-md-9">
+        <div class="row">
+            <div class="col-md-3" style="border-right: black 1px solid;">             
+                <div>
                     ${repeat(this.allParticipants,(item) => html`
-                                                            <div draggable="true" onDragStart="${() => this.dragStart(event)}" id="${item.participantId}">
-                                                                <div class="col-md-12">
-                                                                    <div class="rectangle-Part rconers form-style">
-                                                                        <label class="font-styles-Part">${item.firstName} ${item.lastName}</label>
-                                                                    </div>
+                                                            <div draggable="true" id="${item.participantId}" @dragstart="${()=> this.dragStartHandler(item.participantId)}">
+                                                                <div class="rectangle-Part rconers">
+                                                                    <label class="form-style">${item.firstName} ${item.lastName}</label>
                                                                 </div>
                                                             </div>
                                                             `)}
                 </div>  
             </div>
             <div class="col-md-8">
-                <div id="Appo" class="col-md-7">
+                <div id="Appo" style="padding-left: 50%">
+                
+                    <div class="rectangle-Appo rconers">
+                        <div class="col-md-12">
+                            <label class="form-style col-md-12" style="padding: 10px; text-align: center; border-bottom: #0c5460 1px solid;">Montag</label>
+                        </div>                
+                        <div style="height: 200px;width: 300px;" id="test"  @drop="${() => this.dropHandler()}" >
+                        
+                        </div>
+                    </div>
                     
                 </div>
             </div>
